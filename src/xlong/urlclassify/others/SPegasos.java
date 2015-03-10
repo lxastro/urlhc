f49737307f65af2f595d7b3ac36cb48ee51c3060
Xiang Long <lxastro0@163.com>
2015-03-10 22:27:11 +0800

    weka 3.6 test libsvm

--- src/xlong/urlclassify/others/SPegasos.java
+++ src/xlong/urlclassify/others/SPegasos.java
@@ -1,3 +1,6 @@
+package xlong.urlclassify.others;
+public class SPegasos{}
+
 ///*
 // *    This program is free software; you can redistribute it and/or modify
 // *    it under the terms of the GNU General Public License as published by
